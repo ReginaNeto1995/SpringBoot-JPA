b863edf491a704c4b5ab958fcc720ecab1c8500c
ReginaNeto <116004474+ReginaNeto@users.noreply.github.com>
2024-08-23 15:20:29 +0100

    H2 database, test profile, jpa
    H2 database, test profile, jpa

--- src/main/java/SpringBoot/JPA/JpaApplication.java
+++ src/main/java/SpringBoot/JPA/JpaApplication.java
@@ -1,17 +1,16 @@
 package SpringBoot.JPA;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
+	SpringApplication.run(JpaApplication.class, args);
+			
 	}
 
 }

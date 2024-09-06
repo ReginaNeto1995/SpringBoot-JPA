5aae3c27a8915355215a5616a3602f9b90582806
ReginaNeto <116004474+ReginaNeto@users.noreply.github.com>
2024-09-06 15:33:34 +0100

    Métodos SubTotal e Total de pagamento
    Métodos SubTotal e Total de pagamento

--- src/main/java/SpringBoot/JPA/entities/Payment.java
+++ src/main/java/SpringBoot/JPA/entities/Payment.java
@@ -3,6 +3,8 @@ package SpringBoot.JPA.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Payment implements Serializable{
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId //Colocamos na classe dependente, se não existir order não existe pagamento, por isso pagamento é uma "classe dependente " de order
 	private Order order;
